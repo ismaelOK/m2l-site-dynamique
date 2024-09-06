e4ed4bbd0435f41599d8c733b8fac9751faf929e
Romain MOUROUVIN <rmourouvin@geiffel.fr>
2024-09-06 17:10:18 +0200

    Ajouut du fichier bulletin.php

---
+++ modele/dto/bulletin.php
@@ -0,0 +1,20 @@
+<?php
+class Bulletin{
+    protected string $idBulletin;
+    
+    protected int $mois;
+    
+    protected int $annee;
+
+    protected string $bulletinPDF;
+
+    public function __construct(string $idBulletin, int $mois, int $annee, string $bulletinPDF)
+    {
+        $this-string >idBulletin = string $idBulletin;
+        $this-int >mois = int $mois;
+        $this-int >annee = int $annee;
+        $this-string >bulletinPDF = string $bulletinPDF;
+    }
+
+
+}
